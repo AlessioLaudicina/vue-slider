ba4d11d0a7eff86bc2e45dcb66732a1198e27bbb
Alessio Laudicina <alessiolaudicina1@gmail.com>
2023-03-16 10:10:52 +0100

    aggiunta array di oggetti

--- js/script.js
+++ js/script.js
@@ -35,7 +35,12 @@ const slides = [
 
 console.log(slides);
 
-*/
+
+
+ */
+
+
+// il mio esercizio
 
 const { createApp } = Vue
 
@@ -45,28 +50,31 @@ const { createApp } = Vue
         activeImage: 0,
 
         slides: [
-            "img/01.webp",
-            "img/02.webp",
-            "img/03.webp",
-            "img/04.webp",
-            "img/05.webp"
-        ],
-
-
-        activeTitle: 0,
-
-        titles: [
-             'Marvel\'s Spiderman Miles Morale',
-             'Ratchet & Clank: Rift Apart',
-             'Fortnite',
-             'Stray',
-             'Marvels'
-
-
-        ]
+          
+              {
+                  image: 'img/01.webp',
+                  title: 'Marvel\'s Spiderman Miles Morale',
+                  text: 'Experience the rise of Miles Morales as the new hero masters incredible, explosive new powers to become his own Spider-Man.',
+              }, {
+                  image: 'img/02.webp',
+                  title: 'Ratchet & Clank: Rift Apart',
+                  text: 'Go dimension-hopping with Ratchet and Clank as they take on an evil emperor from another reality.',
+              }, {
+                  image: 'img/03.webp',
+                  title: 'Fortnite',
+                  text: "Grab all of your friends and drop into Epic Games Fortnite, a massive 100 - player face - off that combines looting, crafting, shootouts and chaos.",
+              }, {
+                  image: 'img/04.webp',
+                  title: 'Stray',
+                  text: 'Lost, injured and alone, a stray cat must untangle an ancient mystery to escape a long-forgotten city',
+              }, {
+                  image: 'img/05.webp',
+                  title: "Marvel's Avengers",
+                  text: 'Marvel\'s Avengers is an epic, third-person, action-adventure game that combines an original, cinematic story with single-player and co-operative gameplay.',
+              }
+          ]
       }
     },
-
     methods: {
       gotoPrev(){
         this.activeTitle--;
@@ -92,7 +100,6 @@ const { createApp } = Vue
       changeImage(newActiveImage){
         this.activeImage = newActiveImage
       },
-      
 
       getActiveClass(index){
         if(index == this.activeImage){
@@ -110,4 +117,12 @@ const { createApp } = Vue
         this.autoplay();
     }
 
+
+    
+
   }).mount('#app')
+
+ 
+
+
+  
